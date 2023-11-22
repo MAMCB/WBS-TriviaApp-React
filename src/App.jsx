3d22ac84e11c59a6556115e53c5428d0c84dfd91
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-22 18:19:56 +0100

    tryed to make the API call to work with restrict request limit installed and imported lodash

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,46 @@
 import { useState,useEffect } from 'react'
 import Results from './Components/Results';
 import axios from 'axios';
+import _ from "lodash";
+
 import './App.css'
 
 function App() {
   const [complete, setComplete] = useState(false);
   const [questions,setQuestions] =useState([]);
   const [answers,setAnswers] =useState([]);
-  const [category,setCategory] = useState(0);
+  const [category,setCategory] = useState(1);
+  
 
-  useEffect(()=>{
-    axios.get(`https://opentdb.com/api.php?amount=10&category=${category}`).then(response=>setQuestions(response.data)).catch(e=>console.error(e));
-  },[])
+useEffect(() => {
+  const fetchData = async () => {
+    try {
+      const response = await axios.get(
+        `https://opentdb.com/api.php?amount=10&category=21`
+      );
+      setQuestions(response.data);
+    } catch (error) {
+      if (error.response && error.response.status === 429) {
+        const retryAfter =
+          parseInt(error.response.headers["retry-after"], 10) || 7; // Default to 7 seconds if no valid header
+        console.log(
+          `Rate limit exceeded. Retrying after ${retryAfter} seconds.`
+        );
+
+        // Retry after the specified duration
+        setTimeout(() => {
+          fetchData();
+        }, retryAfter * 1000);
+      } else {
+        console.error(error);
+      }
+    }
+  };
+
+  const delayedFetch = _.throttle(fetchData, 7000); // Throttle to one request per 7 seconds
+
+  delayedFetch();
+}, [category]);
 
   return (
     <>
