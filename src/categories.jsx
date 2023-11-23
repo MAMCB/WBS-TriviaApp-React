f4be0a1d6c314a8d51a99c45c04b2f46894822a3
Andrii <andriikuznichenko@gmail.com>
2023-11-23 12:08:49 +0100

    add demo version of category page

---
+++ src/categories.jsx
@@ -0,0 +1,124 @@
+const categories = [
+  {
+    value: '9',
+    category: 'General Knowledge',
+    img: 'https://media.proprofs.com/images/QM/user_images/2503852/New%20Project%20(61)(211).jpg'
+  },
+  {
+    value: '10',
+    category: 'Books',
+    img: 'https://s26162.pcdn.co/wp-content/uploads/sites/2/2022/05/Best-Reviewed-Books.jpeg'
+  },
+  {
+    value: '11',
+    category: 'Film',
+    img: 'https://college.unc.edu/wp-content/uploads/sites/1280/2020/05/film1.jpg'
+  },
+  {
+    value: '12',
+    category: 'Music',
+    img: 'https://at-cdn-static.audiotool.com/images/homepage/slides/start.jpg?width=1400&height=800&fit=inside&format=webp&v=1700689381363',
+  },
+  {
+    value: '13',
+    category: 'Musicals',
+    img: 'https://www.usatoday.com/gcdn/-mm-/b53630da96927e3c07ffa6b1105a8f494dbecdcb/c=0-172-2133-3016/local/-/media/2016/01/23/USATODAY/usatsports/xxx-zx70366-d-shrek-musical-zx70.jpg',
+  },
+  {
+    value: '14',
+    category: 'Television',
+    img: 'https://assets-prd.ignimgs.com/2023/10/17/daredevilredo4-blogroll-1697569645058.jpg',
+  },
+  {
+    value: '15',
+    category: 'Video Games',
+    img: 'https://media.cnn.com/api/v1/images/stellar/prod/i-stock-1287493837-1.jpg?c=16x9&q=w_800,c_fill',
+  },
+  {
+    value: '16',
+    category: 'Board Games',
+    img: 'https://cdn.thewirecutter.com/wp-content/media/2020/10/smallworldboardgames-2048px-33.jpg?auto=webp&quality=75&width=1024',
+  },
+  {
+    value: '17',
+    category: 'Nature',
+    img: 'https://img.freepik.com/fotos-kostenlos/waldlandschaft_71767-127.jpg?size=626&ext=jpg&ga=GA1.1.386372595.1697673600&semt=ais',
+  },
+  {
+    value: '18',
+    category: 'Computers',
+    img: 'https://history-computer.com/wp-content/uploads/2023/01/shutterstock_2093652733-scaled.jpg',
+  },
+  {
+    value: '19',
+    category: 'Mathematics',
+    img: 'https://www.templeton.org/wp-content/uploads/2023/08/Mathematics-Article-BANNER-1-scaled.jpg',
+  },
+  {
+    value: '20',
+    category: 'Mythology',
+    img: 'https://upload.wikimedia.org/wikipedia/commons/thumb/f/f8/The_Pleiades_%28Elihu_Vedder%29.jpg/1200px-The_Pleiades_%28Elihu_Vedder%29.jpg',
+  },
+  {
+    value: '21',
+    category: 'Sports',
+    img: 'https://www.adesso.de/adesso-de/adesso-de/branchen/sports/sports-header_W850xH425_CUTOUT.jpg',
+  },
+  {
+    value: '22',
+    category: 'Geography',
+    img: 'https://study.com/cimages/videopreview/p3c2j8y73a.jpg',
+  },
+  {
+    value: '23',
+    category: 'History',
+    img: 'https://history.brown.edu/sites/default/files/styles/ultrawide_med/public/2022-09/history.brown_.edu_.jpg?h=959865f0&itok=gvdoC1iT',
+  },
+  {
+    value: '24',
+    category: 'Politics',
+    img: 'https://blog.politics.ox.ac.uk/wp-content/uploads/2023/05/visual-politics-scaled.jpg',
+  },
+  {
+    value: '25',
+    category: 'Art',
+    img: 'https://cdn.britannica.com/78/43678-050-F4DC8D93/Starry-Night-canvas-Vincent-van-Gogh-New-1889.jpg',
+  },
+  {
+    value: '26',
+    category: 'Celebrities',
+    img: 'https://www.thesun.co.uk/wp-content/uploads/2016/10/nintchdbpict000000444356.jpg',
+  },
+  {
+    value: '27',
+    category: 'Animals',
+    img: 'https://res.cloudinary.com/dk-find-out/image/upload/q_80,w_1920,f_auto/Animal_kingdom_nzwbda.jpg',
+  },
+  {
+    value: '28',
+    category: 'Vehicles',
+    img: 'https://imageio.forbes.com/specials-images/imageserve/60c35192ba409e6fc1b4012e/Self-Driving-car/0x0.jpg?format=jpg&crop=5063,2374,x0,y230,safe&width=960',
+  },
+  {
+    value: '29',
+    category: 'Comics',
+    img: 'https://www.komar.de/media/catalog/product/cache/4/image/9df78eab33525d08d6e5fb8d27136e95/1/4/14742.jpg',
+  },
+  {
+    value: '30',
+    category: 'Gadgets',
+    img: 'https://cdn.vox-cdn.com/thumbor/fHlJj4uJ8pHi2osJJb85XsP-Tt8=/0x0:2625x1907/2000x1333/filters:focal(1313x954:1314x955)/cdn1.vox-cdn.com/uploads/chorus_asset/file/6384511/jbareham_160418_0931_0086_FINAL_NO_BUFFER_5MB_02.0.jpg',
+  },
+  {
+    value: '31',
+    category: 'Japanese Anime',
+    img: 'https://japancitytour.com/wp-content/uploads/2020/11/anime-e1694421356436.jpeg',
+  },
+  {
+    value: '32',
+    category: 'Cartoon',
+    img: 'https://wallpapers.com/images/hd/cartoon-pictures-q3hxkk0gmw6vnivj.jpg',
+  },
+];
+
+export default categories;
---
+++ src/components/CategoryGrid.jsx
@@ -0,0 +1,60 @@
+import {useState, useEffect} from 'react'
+import categories from '../categories'
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+
+function CategoryGrid() {
+  
+  const [isResult, setIsResult] = useState(false);
+  const [isQuiz, setIsQuiz] = useState(false);
+  const [category, setCategory] = useState('0');
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    if(category !== '0'){
+      fetch(`https://opentdb.com/api.php?amount=10&category=${category}`)
+      .then(res => res.json())
+      .then(data =>
+        setTimeout(() => {
+          setQuestions(data);
+        }, 1000),
+      )
+      .catch(error => console.error(error));
+    }
+  }, [category]);
+
+
+//  useEffect(() => {
+//   console.log(questions);
+//  }, [questions])
+
+
+  const CardHandler = (e) => {
+    setIsQuiz(true);
+    setCategory(e.currentTarget.id);
+  }
+
+  return (
+    <>
+    {isResult?<p>place for result compoinent</p>
+    :isQuiz?<p>place for questions component</p>
+    :<><h1 className='mb-3'>Choose your category</h1>
+          <Row xs={1} md={3} xl={4} xxl={5} className="g-4">
+            {categories.map(categoryCard => (
+              <Col key={categoryCard.value}>
+                <Card id={categoryCard.value} className='card-category' onClick={CardHandler}>
+                  <Card.Img variant="top" className='category-img' src={categoryCard.img} />
+                  <Card.Body>
+                    <Card.Title>{categoryCard.category}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Col>
+            ))}
+          </Row></>
+    }
+    </>
+  )
+}
+
+export default CategoryGrid
