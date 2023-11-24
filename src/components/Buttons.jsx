00a0ab46a77dfbebf171a146c4e869d0b99a081c
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-24 17:02:13 +0100

    made previous button functionality

--- src/components/Buttons.jsx
+++ src/components/Buttons.jsx
@@ -1,27 +1,58 @@
-const Buttons = ({ selectNextQuestion, selectPreviousQuestion, setSubmit, currentQuestion, questionsLength, setIsChoose, isChoose }) => {
+import { useState, useEffect } from "react";
+
+const Buttons = ({
+  selectNextQuestion,
+  selectPreviousQuestion,
+  setSubmit,
+  currentQuestion,
+  questionsLength,
+  setIsChoose,
+  isChoose,
+}) => {
+  const[previousChoices,setPreviousChoices]=useState([])
+
   const handlePrevious = () => {
     selectPreviousQuestion();
+    
+  
+    setIsChoose([...previousChoices][currentQuestion - 1].choice.isChoose);
   };
 
   const handleNext = () => {
+    const previousChoice ={
+      questionIndex:{currentQuestion},
+      choice:{isChoose}
+    }
+    setPreviousChoices([...previousChoices, previousChoice]);
     selectNextQuestion();
-    setIsChoose([false, false, false, false])
+    
+    setIsChoose([false, false, false, false]);
   };
 
-  const handleSubmit = ()=>{
+  const handleSubmit = () => {
     setSubmit();
-  }
+  };
 
   return (
     <>
-      {!currentQuestion === 0 && (
+      {currentQuestion !== 0 && (
         <button onClick={handlePrevious}>Previous</button>
       )}
       {currentQuestion < questionsLength - 1 && (
-        <button onClick={handleNext} disabled={![...isChoose].includes(true)?true:false}>Next</button>
+        <button
+          onClick={handleNext}
+          disabled={![...isChoose].includes(true) ? true : false}
+        >
+          Next
+        </button>
       )}
       {currentQuestion === questionsLength - 1 && (
-        <button onClick={handleSubmit} disabled={![...isChoose].includes(true)?true:false}>Submit</button>
+        <button
+          onClick={handleSubmit}
+          disabled={![...isChoose].includes(true) ? true : false}
+        >
+          Submit
+        </button>
       )}
     </>
   );
