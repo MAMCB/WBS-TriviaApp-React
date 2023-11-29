00c96f646ea3155f80c89fcbf9ad1fd3c15ef5e2
MAMCB <96302464+MAMCB@users.noreply.github.com>
2023-11-29 10:28:32 +0100

    made answers and ischoose functionality for level1

--- src/components/Level1.jsx
+++ src/components/Level1.jsx
@@ -15,51 +15,62 @@ function Level1() {
   const [isResult, setIsResult] = useState(false);
   const [isQuiz, setIsQuiz] = useState(false);
   const [userAnswers, setUserAnswers] = useState([]); 
-  const [isChoose, setIsChoose] = useState([false, false, false, false]);
+  const [isChoose, setIsChoose] = useState([
+    [false, false, false, false],
+    [false, false, false, false],
+    [false, false, false, false],
+    [false, false, false, false],
+    [false, false, false, false],
+    [false, false, false, false]
+  ]);
 
-console.log(userAnswers);
-  function handleAnswer(e, indexQuestion) {
-    console.log(indexQuestion);
-    console.log(e.target.id);
-    console.log(QuestionsLevel1[indexQuestion].answers[e.target.id]);
-  // const answerThis = [...userAnswers].filter((answer) => {
-  //   return answer.id === e.target.id;
-  // });
-  // if (answerThis.length > 0) {
-  //   e.target.checked = false;
 
-  //   if (!e.target.checked && answerThis[0].answerId === e.currentTarget.id) {
-  //     const deleteAnswer = [...userAnswers].filter((answer) => {
-  //       return answer.id !== e.target.id;
-  //     });
-  //     setUserAnswers(deleteAnswer);
+ function handleAnswer(e, indexQuestion) {
+   const updatedCheckboxes = isChoose.map((checkboxes, questionIndex) =>
+     questionIndex === indexQuestion
+       ? checkboxes.map((checkbox, checkboxIndex) =>
+           checkboxIndex === parseInt(e.target.id) ? !checkbox : checkbox
+         )
+       : checkboxes
+   );
+   
 
-  //     const newChoose = isChoose.map((ele, index) => {
-  //       if (index == answerThis[0].answerId) {
-  //         ele = false;
-  //       }
-  //       return ele;
-  //     });
+   const newAnswer = {
+     questionIndex: indexQuestion,
+     answer: QuestionsLevel1[indexQuestion].answers[e.target.id],
+     isCorrectAnswer:
+       QuestionsLevel1[indexQuestion].answers[e.target.id] ===
+       QuestionsLevel1[indexQuestion].correctAnswer,
+   };
 
-  //     setIsChoose(newChoose);
-  //   }
-  // } else {
-  //   const newAnswer = {
-  //     id: e.target.id,
-  //     answerId: e.currentTarget.id,
-  //     answer: QuestionsLevel1[indexQuestion].answers[e.target.id],
-  //   };
-  //   setUserAnswers([...userAnswers, newAnswer]);
-  //   const newChoose = isChoose.map((ele, index) => {
-  //     if (index == newAnswer.answerId) {
-  //       ele = true;
-  //     }
-  //     return ele;
-  //   });
+   if (userAnswers.find((e) => e.questionIndex === indexQuestion)) {
+     const reMapAnswers = userAnswers
+       .map((answer) =>
+         answer.questionIndex === indexQuestion ? null : answer
+       )
+       .filter(Boolean);
 
-  //   setIsChoose(newChoose);
-  // }
-}
+     if (e.target.checked) {
+       const newUpdate = updatedCheckboxes.map((checkboxes, questionIndex) =>
+         questionIndex === indexQuestion
+           ? checkboxes.map((checkbox, checkboxIndex) =>
+               checkboxIndex !== parseInt(e.target.id) ? checkbox : !checkbox
+             )
+           : checkboxes
+       );
+
+       setIsChoose(newUpdate);
+       setUserAnswers([...reMapAnswers, newAnswer]);
+     } else {
+       setIsChoose(updatedCheckboxes);
+       setUserAnswers(reMapAnswers);
+     }
+   } else {
+     setUserAnswers([...userAnswers, newAnswer]);
+     setIsChoose(updatedCheckboxes);
+   }
+ }
+
 
 
   return (
@@ -75,6 +86,7 @@ console.log(userAnswers);
                     <Form.Check
                       type="checkbox"
                       id={`${answerIndex}`}
+                      checked={isChoose[index][answerIndex]}
                       
                       onChange={(e) => handleAnswer(e,index)}
                     />
